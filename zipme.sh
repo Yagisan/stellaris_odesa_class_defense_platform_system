7b6c6da6b6cffe28c9d7e8b1b17a7ef70bc0ca82
Jamie Jones <yagisan@yagisan.org>
2024-01-24 08:20:18 +0900

    Release utility scripts

---
+++ zipme.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git archive --format=zip -9 -v --add-file=stellaris_odesa_class_defense_platform_system.mod --prefix=stellaris_odesa_class_defense_platform_system/ --output=stellaris_odesa_class_defense_platform_system.zip main
